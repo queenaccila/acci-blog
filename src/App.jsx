8667b3c3249f62341bcfb6e261317076f1b877df
Helen Ho <helenho2k1@gmail.com>
2025-08-22 22:46:12 -0700

    add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { useState } from 'react'
 import acciLogo from './assets/acci-logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <img src={acciLogo} className="acci-logo"/>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Welcome to my blog!</h1>
     </>
   )
 }
