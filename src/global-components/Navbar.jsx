c2ec04ab4594b885496ebcfd147d89944b75b001
Helen Ho <helenho2k1@gmail.com>
2025-08-23 20:23:21 -0700

    added dropdown navbar

--- src/global-components/Navbar.jsx
+++ src/global-components/Navbar.jsx
@@ -5,14 +5,21 @@ function Navbar() {
     <>
       <ul className="navbar">
         <li className="navbar-single-link"><a href="">About</a></li>
-        <li className="navbar-single-link"><a href="">Posts</a></li>
         <li className="navbar-single-link">
-            <a href="">Art</a>
-            <ul className="navbar-dropdown">
-            <li><a href="">Originals</a></li>
-            <li><a href="">Animation</a></li>
-            <li><a href="">Fanart</a></li>
-            </ul>
+          <a href="">Posts</a>
+          <ul className="navbar-dropdown">
+              <li className="navbar-dropdown-link"><a href="">General</a></li>
+              <li className="navbar-dropdown-link"><a href="">Showcase</a></li>
+              <li className="navbar-dropdown-link"><a href="">Devlogs</a></li>
+          </ul>
+        </li>
+        <li className="navbar-single-link">
+          <a href="">Art</a>
+          <ul className="navbar-dropdown">
+            <li className="navbar-dropdown-link"><a href="">Originals</a></li>
+            <li className="navbar-dropdown-link"><a href="">Animation</a></li>
+            <li className="navbar-dropdown-link"><a href="">Fanart</a></li>
+          </ul>
         </li>
         <li className="navbar-single-link"><a href="">Videos</a></li>
         <li className="navbar-single-link"><a href="">Inbox</a></li>
