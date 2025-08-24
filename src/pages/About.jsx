b5e2c55b193989f1c2f65ade8e8e85e6e7c68621
Helen Ho <helenho2k1@gmail.com>
2025-08-24 01:01:56 -0700

    Started making about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,19 @@
 import Navbar from "../global-components/Navbar"
+import './About.css'
 
 function About() {
   return (
     <>
       <Navbar />
+      <div className="title-div">
+        <h1>About My Blog</h1>
+        <div className="section">
+          <h3>Making this website</h3>
+          <p>
+            
+          </p>
+        </div>
+      </div>
     </>
   )
 }
