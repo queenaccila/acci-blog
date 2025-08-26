eec1eccc235f5de8a13c7401c92101199806552c
Helen Ho <helenho2k1@gmail.com>
2025-08-26 00:58:56 -0700

    removed navbar dropdown

--- src/global-components/Navbar.jsx
+++ src/global-components/Navbar.jsx
@@ -8,22 +8,8 @@ function Navbar() {
       <HomeDirect />
       <ul className="navbar">
         <li className="navbar-single-link"><Link to="/about">About</Link></li>
-        <li className="navbar-single-link">
-          <a href="">Posts</a>
-          <ul className="navbar-dropdown">
-              <li className="navbar-dropdown-link"><a href="">General</a></li>
-              <li className="navbar-dropdown-link"><a href="">Showcase</a></li>
-              <li className="navbar-dropdown-link"><a href="">Devlogs</a></li>
-          </ul>
-        </li>
-        <li className="navbar-single-link">
-          <a href="">Art</a>
-          <ul className="navbar-dropdown">
-            <li className="navbar-dropdown-link"><a href="">Originals</a></li>
-            <li className="navbar-dropdown-link"><a href="">Animation</a></li>
-            <li className="navbar-dropdown-link"><a href="">Fanart</a></li>
-          </ul>
-        </li>
+        <li className="navbar-single-link"><a href="">Posts</a></li>
+        <li className="navbar-single-link"><a href="">Art</a></li>
         <li className="navbar-single-link"><a href="">Videos</a></li>
         <li className="navbar-single-link"><a href="">Inbox</a></li>
       </ul>
