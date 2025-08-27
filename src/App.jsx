a7f095d25eb0b989b4ea54122feb5d52f7e59576
Helen Ho <helenho2k1@gmail.com>
2025-08-26 23:54:50 -0700

    added navbar globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Navbar from './global-components/Navbar'
+import Footer from './global-components/Footer'
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />}></Route>
         </Routes>
       </Router>
+      <Footer />
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import acciLogo from '../assets/acci-logo.png'
-import Footer from "../global-components/Footer";
 import './Home.css'
 
 function Home() {
@@ -11,7 +10,6 @@ function Home() {
         <h3 className="description-header">She/Her ✦ 23 ✦ Sep 29th ✦ PST</h3>
         <p className="description-text">My cozy corner of the internet where I share my projects, art, and general thoughts on things I like.</p>
       </div>
-      <Footer />
     </>
   )
 }
