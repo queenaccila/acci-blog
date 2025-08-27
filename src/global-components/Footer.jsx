159f455dbca6681828c4a55df983d555505608e3
Helen Ho <helenho2k1@gmail.com>
2025-08-26 22:44:12 -0700

    added links in blog

--- src/global-components/Footer.jsx
+++ src/global-components/Footer.jsx
@@ -9,14 +9,14 @@ import { IconContext } from "react-icons";
 function Footer() {
   return (
     <div className="entire-footer">
-        <p className="copyright-text">© 2025 - Accila</p>
+        <p className="copyright-text">© 2025 - Accila / Helen Ho</p>
         <ul className="social-media">
             <IconContext.Provider value={{ size: "1.5rem" }}>
-            <li className="social-media-link"><a href=""><FaGithub /></a></li>
-            <li className="social-media-link"><a href=""><FaItchIo /></a></li>
-            <li className="social-media-link"><a href=""><FaYoutube /></a></li>
-            <li className="social-media-link"><a href=""><FaBluesky /></a></li>
-            <li className="social-media-link"><a href=""><FaLinkedin /></a></li>
+            <li className="social-media-link"><a href="https://github.com/queenaccila"><FaGithub /></a></li>
+            <li className="social-media-link"><a href="https://queenaccila.itch.io/"><FaItchIo /></a></li>
+            <li className="social-media-link"><a href="https://www.youtube.com/@accila"><FaYoutube /></a></li>
+            <li className="social-media-link"><a href="https://bsky.app/profile/queenaccila.bsky.social"><FaBluesky /></a></li>
+            <li className="social-media-link"><a href="https://www.linkedin.com/in/helen-ho-5a496724a/"><FaLinkedin /></a></li>
             </IconContext.Provider>
         </ul>
     </div>
