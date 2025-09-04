86e6ec989204e439eab662ab30ca3e2e629907e7
Helen Ho <helenho2k1@gmail.com>
2025-09-04 00:01:08 -0700

    Added inbox submission display

--- src/global-components/InboxSubmission.jsx
+++ src/global-components/InboxSubmission.jsx
@@ -1,29 +1,46 @@
+// InboxSubmissionBubble.jsx
 import { Link } from "react-router-dom";
+import { BsPersonFill } from "react-icons/bs";
+import './InboxSubmission.css';
 
 function InboxSubmission({
-    text, submissionImg, username, userUrl, userIcon
+    text,
+    submissionImg,
+    username,
+    userUrl,
+    userIcon
 }) {
     const isNotAnon = !!username && !!userUrl && !!userIcon;
+    const defaultIcon = <BsPersonFill />; // replace with your default
 
     return (
-        <div className={`submission ${isNotAnon ? "with-user" : "simple"}`}>
-            {/* main submission text */}
-            <div>
-                <p>{text}</p>
-            </div>
-
-            {/* optional image submission */}
-            {submissionImg && (
-                <img src={submissionImg}/>
-            )}
-            
-            {/* non-anon submissions */}
-            {isNotAnon && (
-                <div>
-                    <img src={userIcon}/>
-                    <p><Link to={userUrl}>{username}</Link></p>
+        <div>
+            <div className="bubble-container">
+                <div className="bubble-user">
+                    <img
+                        src={isNotAnon ? userIcon : defaultIcon}
+                        alt={isNotAnon ? username : "Anonymous"}
+                        className="bubble-user-icon"
+                    />
+                    <div className="bubble-username">
+                        {isNotAnon ? (
+                            <Link to={userUrl} className="user-name">{username}</Link>
+                        ) : (
+                            <span className="user-name">Anonymous</span>
+                        )}
+                    </div>
                 </div>
-            )}
+
+                <div className="bubble">
+                    <div className="bubble-text">
+                        <p>{text}</p>
+                        {submissionImg && <img src={submissionImg} alt="Submission" className="bubble-image"/>}
+                    </div>
+                </div>
+            </div>
+            <hr className="space-divider"/>
         </div>
     );
 }
+
+export default InboxSubmission;
