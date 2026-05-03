460ffa51cb7e0457f75f78586f21bc3aefef8b10
Helen Ho <helenho2k1@gmail.com>
2026-05-02 19:32:04 -0700

    april update post

--- src/global-components/Footer.jsx
+++ src/global-components/Footer.jsx
@@ -5,6 +5,7 @@ import { FaBluesky } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { MdPayments } from "react-icons/md";
+import { SiKofi } from "react-icons/si";
 import { IconContext } from "react-icons";
 
 function Footer() {
@@ -16,9 +17,10 @@ function Footer() {
             <li className="social-media-link"><a href="https://github.com/queenaccila" target="_blank" rel="noopener noreferrer"><FaGithub /></a></li>
             <li className="social-media-link"><a href="https://queenaccila.itch.io/" target="_blank" rel="noopener noreferrer"><FaItchIo /></a></li>
             <li className="social-media-link"><a href="https://vgen.co/queenaccila" target="_blank" rel="noopener noreferrer"><MdPayments /></a></li>
+            <li className="social-media-link"><a href="https://ko-fi.com/queenaccila" target="_blank" rel="noopener noreferrer"><SiKofi /></a></li>
             <li className="newgrounds-logo"><a href="https://queenaccila.newgrounds.com/" target="_blank" rel="noopener noreferrer">NG</a></li>
             <li className="social-media-link"><a href="https://bsky.app/profile/queenaccila.bsky.social" target="_blank" rel="noopener noreferrer"><FaBluesky /></a></li>
-            <li className="social-media-link"><a href="https://www.instagram.com/queen.accila/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></li>
+            <li className="social-media-link"><a href="https://www.instagram.com/queenaccila/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a></li>
             <li className="social-media-link"><a href="https://www.youtube.com/@queenaccila" target="_blank" rel="noopener noreferrer"><FaYoutube /></a></li>
             </IconContext.Provider>
         </ul>
